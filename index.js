7144c23d411edfaf675309bfb1dd66170ade2bd6
DB <diego.botia@udea.edu.co>
2022-06-18 20:05:01 -0500

    Commit 1

---
+++ index.js
@@ -0,0 +1,12 @@
+const interface = require('readline-sync');
+var calculator = require('./app/calculator');
+
+const firstNum= interface.question("Ingrese el primer numero: ");
+const secondNum= interface.question("Ingrese el segundo numero: ");
+
+var a=parseInt(firstNum);
+var b=parseInt(secondNum);
+ console.log(`Add ${a} + ${b} = ${calculator.add(a,b)}`);
+ console.log(`Minus ${a} - ${b} = ${calculator.minus(a,b)}`);
+ console.log(`Multiply ${a} * ${b} = ${calculator.multiply(a,b)}`);
+ console.log(`Divide ${a} / ${b} = ${calculator.divide(a,b)}`);
